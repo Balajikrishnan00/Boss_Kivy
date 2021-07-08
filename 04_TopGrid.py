145da556133e9b1e48f90058ee7ca7dc50a2cfdc
balaji <balajikrishnan00@gmail.com>
2021-07-08 22:02:53 +0300

    updated

--- 04_TopGrid.py
+++ 04_TopGrid.py
@@ -1,18 +1,23 @@
 from kivy.app import App
 from kivy.base import Builder
+
 KV="""
+
 GridLayout:
     cols:1
+    row_force_default:True
+    row_default_height:80
+    col_force_default:True
+    col_default_width:200
     GridLayout:
         cols:2
-        row_force_default:True
-        row_default_height:50
+        
         Label:
             text:'Name'
             bold:True
         TextInput:
             multiline:False
-            id:'name'
+            id:'Name'
         Label:
             text:'Pizza'
         TextInput:
@@ -21,9 +26,9 @@ GridLayout:
         TextInput:
     Button:
         text:'Submit'
-        size_hint:(None,None)
-        width:200
-        height:50
+        #size_hint:(None,None)
+        #width:200
+        #height:50
         pos_hint:{'center_x':.5,'center_y':.5}
         
             
