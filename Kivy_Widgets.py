b7b4ae71f52d05b3d0a747ba60962a886d7f0d63
balaji <balajikrishnan00@gmail.com>
2021-07-20 22:22:09 +0300

    updated

--- Kivy_Widgets.py
+++ Kivy_Widgets.py
@@ -1,8 +1,16 @@
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
+from kivy.properties import StringProperty
 
 class widgetsExample(GridLayout):
-    pass
+    my_text=StringProperty('HEllO')
+    count=1
+    def on_button_click(self):
+
+        print('Button Clicked')
+        self.my_text=str(self.count)
+        self.count+=1
+
 
 
 class testMainApp(App):
